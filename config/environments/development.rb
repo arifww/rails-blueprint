39c02573d6acdfbc8ee443d4bca5c3302461b02d
arifww <arif.w.w@gmail.com>
2019-03-20 04:48:36 +0700

    Update development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.action_controller.asset_host = "http://localhost:3000"
+  #config.action_controller.asset_host = "http://localhost:3000"
 
   config.action_mailer.default_url_options = { :host => "localhost:3000" }
   config.action_mailer.delivery_method = :letter_opener
